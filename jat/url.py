d91e9e6a14cc9bc5b2dd65a841fd67895b91dc00
ryung <johyunryung@naver.com>
2021-06-02 10:02:27 +0900

    jat/urls repository_add

--- jat/url.py
+++ jat/url.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.RepositoryListView.as_view(), name='repository_list'),
     path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),
     path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(), name='introduction_detail'),
+    path('repository/add/', views.RepositoryCreateView.as_view(), name='repository_add'),
 ]
