55ddb02767956443685fb3b24a9cdadaf943e814
ryung <johyunryung@naver.com>
2021-06-02 09:15:04 +0900

    jat/urls introduction_detail

--- jat/url.py
+++ jat/url.py
@@ -6,5 +6,6 @@ app_name = 'jat'
 
 urlpatterns = [
     path('', views.RepositoryListView.as_view(), name='repository_list'),
-    path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail')
+    path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),
+    path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(), name='introduction_detail'),
 ]
