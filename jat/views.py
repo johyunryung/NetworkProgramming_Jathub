a3eda990a7bd42960674f77ba9e00b29233f6e00
ryung <johyunryung@naver.com>
2021-06-02 10:14:05 +0900

    jat/views repositoryUpdateView, repositorydeleteView

--- jat/views.py
+++ jat/views.py
@@ -17,6 +17,16 @@ class RepositoryCreateView(generic.CreateView):
     template_name_suffix = '_create'
     success_url = reverse_lazy('jat:repository_list')
 
+class RepositoryUpdateView(generic.UpdateView):
+    model =  Repository
+    fields = ['name', 'description','deadline']
+    template_name_suffix = '_update'
+    success_url = reverse_lazy('jat:repository_list')
+
+class RepositoryDeleteView(generic.DeleteView):
+    model = Repository
+    success_url = reverse_lazy('jat:repository_list')
+
 class IntroductionDetailView(generic.DetailView):
     model = Introduction
 
