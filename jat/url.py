f23223e1c9ff1950986e2496e4093f57ff39d4f6
ryung <johyunryung@naver.com>
2021-06-02 10:15:24 +0900

    jat/urls repository_modify, repository_delete

--- jat/url.py
+++ jat/url.py
@@ -7,6 +7,8 @@ app_name = 'jat'
 urlpatterns = [
     path('', views.RepositoryListView.as_view(), name='repository_list'),
     path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),
+    path('repository/<int:pk>/modify/', views.RepositoryUpdateView.as_view(), name='repository_modify'),
+    path('repository/<int:pk>/delete/', views.RepositoryDeleteView.as_view(), name='repository_delete'),
     path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(), name='introduction_detail'),
     path('repository/add/', views.RepositoryCreateView.as_view(), name='repository_add'),
 ]
